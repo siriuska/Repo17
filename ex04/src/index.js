5ff195d5d5e55e6a7b186978fb663fb72a091afb
siriuska <you@example.com>
2022-04-13 20:52:34 +0200

    Destructuring - nested objects

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+const BAKING_TEMPERATURES = {
+    cake: { low: 180, high: 250 },
+    bread: { low: 150, high: 220 },
+    pie: { loww: 200, high: 230 }
+};
+
+function main(baking) {
+    const { bread: { low: lowBread, high: highBread } } = baking;
+    return { lowBread, highBread };
+}
+
+console.log(main(BAKING_TEMPERATURES));
+module.exports = main;
