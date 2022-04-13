42b19a65f9ad424baf380a17f72c091ef29b768c
siriuska <you@example.com>
2022-04-13 20:30:54 +0200

    Spread operator

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+const workDays = ['MON', 'TUE', 'WED', 'THU', 'FRI'];
+
+function myWeek(days) {
+    let week = [...days, 'SAT', 'SUN'];
+    return week;
+}
+
+console.log(myWeek(workDays));
+
+module.exports = myWeek;
